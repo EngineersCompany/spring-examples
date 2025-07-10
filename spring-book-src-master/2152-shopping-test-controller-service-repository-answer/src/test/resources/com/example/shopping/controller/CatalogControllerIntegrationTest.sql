b0729f5aa15bed52884fc44d7e69791d3e53f648
Tetsuzo <Tetsuzo@192.168.1.8>
2025-07-10 19:41:43 +0900

    不要PJの削除

--- spring-book-src-master/2152-shopping-test-controller-service-repository-answer/src/test/resources/com/example/shopping/controller/CatalogControllerIntegrationTest.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO t_product
-(id, name, price, stock) VALUES
-('p01', '消しゴム', 100, 10)
-,('p02', 'ノート', 200, 20)
-;
-
--- spring-book-src-master/2302-shopping-selenide-answer/src/test/resources/com/example/shopping/ui/clear.sql
+++
@@ -1,3 +0,0 @@
-delete from t_order_item;
-delete from t_order;
-delete from t_product;
